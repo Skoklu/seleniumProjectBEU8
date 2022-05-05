471b165d911efbbef88fe20df3912f31eddab4b5
selma <sunal.32@hotmail.com>
2022-05-05 12:21:36 +0100

    day2 Task1_CydeoTitleVerification

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -15,11 +15,24 @@ public class T1_CydeoVerifications {
         driver.manage().window().maximize();
 
 
-
-
         //2. Go to https://practice.cydeo.com
+        driver.navigate().to("https://practice.cydeo.com");
+
+
+
         //3. Verify URL contains
         //Expected: cydeo
+         String expectedURL = "cydeo";
+         String actualURL = driver.getCurrentUrl();
+
+         if (actualURL.equals(expectedURL)){
+             System.out.println("URL verification passed");
+         }else{
+             System.out.println("URL verification is Failed!!!");
+         }
+
+
+
        // 4. Verify title:
        // Expected: Practice
 
