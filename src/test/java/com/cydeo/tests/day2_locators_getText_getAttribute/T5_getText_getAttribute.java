d1ddc28e81473cdeb29041f683ec2f4c989b7e00
selma <sunal.32@hotmail.com>
2022-05-06 00:52:04 +0100

    day2 Task5_getText_getAttribute

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_getText_getAttribute.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T5_getText_getAttribute.java
@@ -36,9 +36,18 @@ public class T5_getText_getAttribute {
         }
 
 
-
-
         // 4- Locate “First name” input box
+        WebElement firstNameInput = driver.findElement(By.name("firstname"));
+
+
         //5- Verify placeholder attribute’s value is as expected: Expected: first name
+        String expectedPlaceHolder = "first name";
+        String actualPlaceHolder = firstNameInput.getAttribute("placeholder");
+
+        if (actualPlaceHolder.equals(expectedPlaceHolder)){
+            System.out.println("Placeholder text verification PASSED");
+        }else {
+            System.out.println("Placeholder text verification FAILED!!!");
+        }
     }
 }
