5d3a4a61299d5b45d80a7d277aa82d7ed4d4966a
selma <sunal.32@hotmail.com>
2022-05-12 22:05:31 +0100

    day4 Task1_xpath_cssSelector practice

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T1_xpath_cssSelector_practice.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day4_findElements_checkboxes_radio;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class T1_xpath_cssSelector_practice {
 
@@ -18,14 +20,66 @@ public class T1_xpath_cssSelector_practice {
 
         // 2. Go to https://practice.cydeo.com/forgot_password
         driver.navigate().to("https://practice.cydeo.com/forgot_password");
+
+
         // 3. Locate all the WebElements on the page using XPATH and/or CSS locator only (total of 6)
-       //  a. “Home” link
+        //  a. “Home” link
+        //Locate homeLink using cssSelector 1, class value
+        WebElement homeLink_ex1 = driver.findElement(By.cssSelector("a[class='nav-link']"));
+
+        //Locate homeLink using cssSelector syntax #2
+        WebElement homeLink_ex2 = driver.findElement(By.cssSelector("a.nav-link"));
+
+        //Locate homeLink using cssSelector 3, href value
+        WebElement homeLink_ex3 = driver.findElement(By.cssSelector("a[href='/']"));
+
+
+
         // b. “Forgot password” header
+        //Locate header using cssSelector: locate parent element and move down to h2
+        WebElement forgotPasswordHeader1 = driver.findElement(By.cssSelector("div.example > h2"));
+
+        // Locate header using xpath, and using web elements text "Forgot Password"
+        //WebElement forgotPasswordHeader2 = driver.findElement(By.xpath("//h2[.='Forgot Password']"));
+        WebElement forgotPasswordHeader3 = driver.findElement(By.xpath("//h2[text()='Forgot Password']"));
+
+
         // c. “E-mail” text
+        WebElement emailLabel  = driver.findElement(By.xpath("//label[@for='email']"));
+
+
         // d. E-mail input box
-        // e. “Retrieve password” button f. “Powered by Cydeo text
+
+        WebElement emailInputBox = driver.findElement(By.xpath("//input[@type='text']"));
+
+        //Locate inputBox using xpath contains method
+
+        //tagName[contains(@attribute,'value')]
+
+        WebElement emailInputBox2 = driver.findElement(By.xpath("//input[@name='email']"));
+
+        //tagName[contains(@attribute,'value')]
+        WebElement emailInputBox3 = driver.findElement(By.xpath("//input[contains(@pattern,'a-z')]"));
+
+
+        // e. “Retrieve password” button
+        WebElement RetrievePasswordButton1 = driver.findElement(By.xpath("//button[@id='form_submit']"));
+        WebElement RetrievePasswordButton2 = driver.findElement(By.xpath("//button[@class='radius']"));
+        WebElement RetrievePasswordButton3 = driver.findElement(By.xpath("//button[@type='submit']"));
+
+
+        // f. “Powered by Cydeo text
+        WebElement PoweredByCydeoLink = driver.findElement(By.xpath("//div[@style='text-align: center;']"));
+
         // 4. Verify all web elements are displayed.
-       // First solve the task with using cssSelector only.
+        System.out.println("homeLink_ex1.isDisplayed() = " + homeLink_ex1.isDisplayed());
+        System.out.println("forgotPasswordHeader1.isDisplayed() = " + forgotPasswordHeader1.isDisplayed());
+        System.out.println("emailInputBox.isDisplayed() = " + emailInputBox.isDisplayed());
+        System.out.println("emailInputBox.isDisplayed() = " + emailInputBox.isDisplayed());
+        System.out.println("RetrievePasswordButton2.isDisplayed() = " + RetrievePasswordButton2.isDisplayed());
+        System.out.println("PoweredByCydeoLink.isDisplayed() = " + PoweredByCydeoLink.isDisplayed());
+
+        // First solve the task with using cssSelector only.
         // Try to create 2 different
         // cssSelector if possible
        // Then solve the task using XPATH only. Try to create 2 different XPATH locator if possible
