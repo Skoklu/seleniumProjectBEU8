479bee58c06a540bd7a558daaa2851b85d1e9906
selma <sunal.32@hotmail.com>
2022-05-13 11:34:36 +0100

    day4 Task5_Checkboxes

--- src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T5_Checkboxes.java
+++ src/test/java/com/cydeo/tests/day4_findElements_checkboxes_radio/T5_Checkboxes.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day4_findElements_checkboxes_radio;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,11 +20,35 @@ public class T5_Checkboxes {
         driver.navigate().to("http://practice.cydeo.com/checkboxes");
 
 
+
+        //We have to locate checkboxes
+        WebElement checkboxes1 = driver.findElement(By.xpath("//input[@name='checkbox1']"));
+        WebElement checkboxes2 = driver.findElement(By.xpath("//input[@name='checkbox2']"));
+
        // 2. Confirm checkbox #1 is NOT selected by default
+        System.out.println("checkboxes1.isSelected() = " + checkboxes1.isSelected());
+
+
+
         // 3. Confirm checkbox #2 is SELECTED by default.
-       // 4. Click checkbox #1 to select it.
+        System.out.println("checkboxes2.isSelected() = " + checkboxes2.isSelected());
+
+
+
+        // 4. Click checkbox #1 to select it.
+        checkboxes1.click();
+
+
        // 5. Click checkbox #2 to deselect it.
+        checkboxes2.click();
+
+
+
         //6. Confirm checkbox #1 is SELECTED.
-       // 7. Confirm checkbox #2 is NOT selected.
+        System.out.println("checkboxes1.isSelected(), expecting true = " + checkboxes1.isSelected());
+
+
+        // 7. Confirm checkbox #2 is NOT selected.
+        System.out.println("checkboxes2.isSelected(), expecting false = " + checkboxes2.isSelected());
     }
 }
