2ef8dad686769fb7186e81a8a5f31819ff8547ea
selma <sunal.32@hotmail.com>
2022-05-26 13:02:25 +0100

    day7 BrowserUtils

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -1,8 +1,13 @@
 package com.cydeo.utilities;
 
 /*
-In this class only general utility methods that are not related to some spesific page.
+In this class only general utility methods that are not related to some specific page.
  */
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
 
 public class BrowserUtils {
 
@@ -17,5 +22,23 @@ public class BrowserUtils {
         }catch (InterruptedException e){
 
         }
+    }
+
+    public static void switchWindowVerify(WebDriver driver, String expectedURL,String expectedInTitle){
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        for (String each : driver.getWindowHandles()) {
+            driver.switchTo().window(each);
+            System.out.println("Current URL: "+ driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains(expectedURL)){
+                break;
+            }
+        }
+
+        // 5. Assert:Title contains“Etsy”
+
+        String actualTitle= driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+
     }
 }
