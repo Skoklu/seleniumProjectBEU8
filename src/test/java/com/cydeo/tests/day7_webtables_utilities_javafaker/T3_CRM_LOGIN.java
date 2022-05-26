a8d9549d958172709e690ae270ebd000244a297f
selma <sunal.32@hotmail.com>
2022-05-26 17:35:00 +0100

    day7 T3_CRM_LOGIN

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,6 +1,7 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
 import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.CRM_Utilities;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -58,8 +59,8 @@ public class T3_CRM_LOGIN {
         //2. Goto:http://login1.nextbasecrm.com/
         driver.get("https://login1.nextbasecrm.com/");
 
-
-
+        //Calling my utility method the login helpdesk1
+        CRM_Utilities.crm_login(driver);
 
         //6. Verify title is as expected:
         //Expected: Portal
