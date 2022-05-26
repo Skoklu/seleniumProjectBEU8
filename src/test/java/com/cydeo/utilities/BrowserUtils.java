7fe18415679a00aecbe420ffd17bfc84aff9348d
selma <sunal.32@hotmail.com>
2022-05-26 13:38:30 +0100

    day7 BrowserUtils

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -46,7 +46,6 @@ public class BrowserUtils {
         }
 
         // 5. Assert:Title contains“expectedInTitle”
-
         String actualTitle= driver.getTitle();
         Assert.assertTrue(actualTitle.contains(expectedInTitle));
 
