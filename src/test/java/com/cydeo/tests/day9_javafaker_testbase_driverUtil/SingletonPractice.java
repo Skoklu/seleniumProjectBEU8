0211af92978a477cc98f1afeb117ceec430f12f2
selma <sunal.32@hotmail.com>
2022-06-07 19:15:27 +0100

    day9_Singleton_practice_test1

---
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/SingletonPractice.java
@@ -0,0 +1,13 @@
+package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
+
+import org.testng.annotations.Test;
+
+public class SingletonPractice {
+
+    @Test
+    public void singleton_understand_test1(){
+
+
+
+    }
+}
