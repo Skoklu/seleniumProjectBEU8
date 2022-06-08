a4c105c69ae8358fc14e85629b05a7bd63939173
selma <sunal.32@hotmail.com>
2022-06-08 07:24:28 +0100

    day10_T1_Registration_Form

---
+++ src/test/java/com/cydeo/tests/day10_upload_actions_isexecuter/T1_Registration_Form.java
@@ -0,0 +1,11 @@
+package com.cydeo.tests.day10_upload_actions_isexecuter;
+
+import org.testng.annotations.Test;
+
+public class T1_Registration_Form {
+
+    @Test
+    public void registration_form_test(){
+
+    }
+}
