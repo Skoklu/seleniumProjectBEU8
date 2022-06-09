0975166f66bf0c9c372e99064fa2e9b4897228c3
selma <sunal.32@hotmail.com>
2022-06-09 10:19:53 +0100

    day1_T4-5_ActionPractice

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractice.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/ActionsPractice.java
@@ -34,5 +34,13 @@ public class ActionsPractice {
 
         // 5- Scroll back up to “Home” link using PageUP button
         actions.sendKeys(Keys.PAGE_UP, Keys.PAGE_UP).perform();
+        //Driver.getDriver().quit();
+        Driver.closeDriver();
+    }
+
+    @Test
+    public void test2(){
+        Driver.getDriver().get("https://practice.cydeo.com/");
+        Driver.closeDriver();
     }
 }
--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -20,7 +20,7 @@ public class Driver {
     We make it static because we will use it in a static method.
 
      */
-    private static WebDriver driver;
+    private static WebDriver driver;  //Value is null by default
 
     /*
     Create re-usable utility method which will return same driver instance when we call it
@@ -57,7 +57,14 @@ public class Driver {
     }
     return driver;
   }
-  //driver.quit() -->nosuchsession
-  //driver.close-->
-  //try to create a method named closeDriver
+  /*
+  This method will make sure our driver value is always null after using quit() method
+   */
+  public static void closeDriver(){
+    if (driver != null){
+      driver.quit();  //this line is terminate the existing session. value will not even be null
+      driver = null;
+    }
+
+  }
 }
