7b7c68a73d48ffdbfc10b04bac54f5b7f42ba741
selma <sunal.32@hotmail.com>
2022-06-10 08:39:53 +0100

    day12_POM Practice

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/POMPractices.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 
 public class POMPractices {
 
+    LibraryLoginPage libraryLoginPage;
     @Test
     public void RequiredFieldErrorMessage(){
 
@@ -18,7 +19,7 @@ public class POMPractices {
 
         // 3- Do not enter any information
         // 4- Click to “Sign in” button
-        LibraryLoginPage libraryLoginPage = new LibraryLoginPage();
+        libraryLoginPage  = new LibraryLoginPage();
 
         libraryLoginPage.signInButton.click();
 
@@ -27,8 +28,32 @@ public class POMPractices {
        // Expected: This field is required. NOTE: FOLLOW POM DESIGN PATTERN
 
         Assert.assertTrue(libraryLoginPage.fieldRequiredErrorMessage.isDisplayed());
+        Driver.closeDriver();
+
+    }
+
+    @Test
+    public void InvalidEmailFormatErrorMessageTest1(){
+
+       // TC #2: Invalid email format error message test
+        // 1- Open a chrome browser
+        //2- Go to: https://library1.cydeo.com
+        Driver.getDriver().get("https://library1.cydeo.com/");
+
+
+        //3- Enter invalid email format
+        libraryLoginPage = new LibraryLoginPage();
+
+        libraryLoginPage.inputUsername.sendKeys("somethingWrong");
+
+
+
+       // 4- Verify expected error is displayed:
+        //Expected: Please enter a valid email address.
+
 
     }
 
 
 }
+//NOTE: FOLLOW POM DESIGN PATTERN
